45f1abbd144b29fd2dfd55f0c3589a9f18ba646f
celsius narhwal <hello@celsiusnarhwal.dev>
2023-05-16 20:54:10 -0400

    add deprecation warning for `iconocards`

--- iconoclast/plugins/iconocards.py
+++ iconoclast/plugins/iconocards.py
@@ -1,9 +1,33 @@
+import logging
+
+import semver
+from importlib_metadata import version
 from material.plugins.social.plugin import SocialPlugin
+from mkdocs.commands.build import DuplicateFilter
 from mkdocs.config.defaults import MkDocsConfig
 from path import Path
 
 
 class IconocardsPlugin(SocialPlugin):
+    def on_config(self, config):
+        public_deprecation = semver.Version.parse(
+            "999.999.999"  # placeholder until public version gets rewritten social plugin
+        )
+        insiders_deprecation = semver.Version.parse("4.33.0")
+
+        theme_version = semver.Version.parse(version("mkdocs-material"))
+
+        public = f"{theme_version.major}.{theme_version.minor}.{theme_version.patch}"
+        insiders = getattr(theme_version, "build", "insiders.0.0.0").lstrip("insiders.")
+
+        if public > public_deprecation or insiders > insiders_deprecation:
+            log.warning(
+                "The iconocards plugin is deprecated for your version of Material for MkDocs. "
+                "Use the built-in social plugin instead."
+            )
+
+        return super().on_config(config)
+
     def _load_logo(self, config: MkDocsConfig):
         theme = config.theme
         icon = theme["icon"]["logo"]
@@ -15,3 +39,7 @@ class IconocardsPlugin(SocialPlugin):
                     return self._load_logo_svg(icon_path, self.color["text"])
 
         return super()._load_logo(config)
+
+
+log = logging.getLogger("mkdocs")
+log.addFilter(DuplicateFilter())
