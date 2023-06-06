fc969d65a5f00d33cd6f1a9f07f2ac556b6f890d
celsius narhwal <hello@celsiusnarhwal.dev>
2023-06-06 10:02:07 -0400

    update __init__.py

--- iconoclast/plugins/__init__.py
+++ iconoclast/plugins/__init__.py
@@ -1,9 +1,1 @@
-import os
-from contextlib import contextmanager
-
-from rich.console import Console
-
-
-@contextmanager
-def new_console() -> Console:
-    yield Console(file=open(os.devnull, "w"), record=True)
+from iconoclast.plugins import utils
